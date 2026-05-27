b66a28b55d1c6d7f359252f9e4453e909dbc0245
Shubham <shubhamrozers@gmail.com>
2026-05-28 00:15:56 +0530

    Implemented JWT Authenication filter

--- auth-service/src/main/java/com/ecommerce/auth/service/CustomUserDetailsService.java
+++ auth-service/src/main/java/com/ecommerce/auth/service/CustomUserDetailsService.java
@@ -8,13 +8,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.yaml.snakeyaml.scanner.ScannerImpl;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class CustomerUserDetailsService implements UserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
--- auth-service/src/main/java/com/ecommerce/auth/service/JwtService.java
+++ auth-service/src/main/java/com/ecommerce/auth/service/JwtService.java
@@ -46,4 +46,9 @@ public class JwtService {
         return claims.getSubject();
     }
 
+    public boolean isTokenValid(String token, String email) {
+        final String username = extractUsername(token);
+        return username.equals(email);
+    }
+
 }
